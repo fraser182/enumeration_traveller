24c5655525c2ae1a47ec5c11304b45917bb104d1
Fraser Ross <fraser182@icloud.com>
2018-12-19 16:53:37 +0000

    6 passing

--- models/traveller.js
+++ models/traveller.js
@@ -9,6 +9,9 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((jounrey) => {
+    return jounrey.endLocation;
+  })
 
 };
 
