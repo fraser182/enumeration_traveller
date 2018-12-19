7c4fba20e74e6dd51039f893f6afbf75a6166669
Fraser Ross <fraser182@icloud.com>
2018-12-19 20:54:15 +0000

    MVP complete - nearly there with extensions

--- models/traveller.js
+++ models/traveller.js
@@ -12,23 +12,18 @@ Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map((journey) => {
     return journey.endLocation;
   })
-
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
   return this.journeys.filter((journey) => {
     return journey.transport === transport;
-
   })
-
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
   return this.journeys.filter((journey) => {
     return journey.distance >= minDistance;
-
   })
-
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
@@ -38,7 +33,10 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  return this.journeys.map((journey) => {
+    return journey.transport;
+  });
+  // filter
 };
 
 
